43d09db59f4e565ae74b70df824657af4cd1d142
joevansanttos <joevansanttos@gmail.com>
2023-04-05 20:47:23 -0300

    quebrar tudo

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -8,10 +8,11 @@ class ClienteSerializer(serializers.ModelSerializer):
         fields = '__all__'
     def validate(self, data):
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({'cpf': "O CPF deve ter 11 digitos!!"})
+            raise serializers.ValidationError({'cpf':"Número de CPF inválido"})
         if not nome_valido(data['nome']):
-            raise serializers.ValidationError({'nome': "Nao inclua numeros nesse nome"})
+            raise serializers.ValidationError({'nome':"Não inclua números neste campo"})
         if not rg_valido(data['rg']):
+<<<<<<< HEAD
             raise serializers.ValidationError({'rg': "O Rg deve ter 9 digitos"})
         if not celular_valido(data['celular']):
             raise serializers.ValidationError({'celular': "O celular deve ter o seguinte formato 71 99783-8747"})
@@ -20,6 +21,13 @@ class ClienteSerializer(serializers.ModelSerializer):
     
 
 
+=======
+            raise serializers.ValidationError({'rg':"O RG deve ter 9 dígitos"})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':"O número de celular deve seguir este modelo: 11 91234-1234 (respeitando os espaços e traço)"})
+        return data
+
+>>>>>>> 524d4ba (adding new validatords)
 
 
 
--- clientes/validators.py
+++ clientes/validators.py
@@ -1,7 +1,15 @@
 import re
+<<<<<<< HEAD
 
 def cpf_valido(numero_do_cpf):
     return len(numero_do_cpf) == 11
+=======
+from validate_docbr import CPF
+
+def cpf_valido(numero_do_cpf):
+    cpf = CPF()
+    return cpf.validate(numero_do_cpf)
+>>>>>>> 524d4ba (adding new validatords)
 
 def nome_valido(nome):
     return nome.isalpha()
@@ -10,8 +18,15 @@ def rg_valido(numero_do_rg):
     return len(numero_do_rg) == 9
 
 def celular_valido(numero_celular):
+<<<<<<< HEAD
     """Verifica se o celular é valido"""
     modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
     resposta = re.findall(modelo, numero_celular)
     return resposta
         
+=======
+    """Verifica se o  celular é valido (11 91234-1234)"""
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    resposta = re.findall(modelo, numero_celular)
+    return resposta
+>>>>>>> 524d4ba (adding new validatords)
