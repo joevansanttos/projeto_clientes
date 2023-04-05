544a451dd8da41b522f96f3e44e72e34e4062976
joevansanttos <joevansanttos@gmail.com>
2023-04-05 20:51:28 -0300

    adding

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -12,22 +12,11 @@ class ClienteSerializer(serializers.ModelSerializer):
         if not nome_valido(data['nome']):
             raise serializers.ValidationError({'nome':"Não inclua números neste campo"})
         if not rg_valido(data['rg']):
-<<<<<<< HEAD
-            raise serializers.ValidationError({'rg': "O Rg deve ter 9 digitos"})
-        if not celular_valido(data['celular']):
-            raise serializers.ValidationError({'celular': "O celular deve ter o seguinte formato 71 99783-8747"})
-        return data
-
-    
-
-
-=======
             raise serializers.ValidationError({'rg':"O RG deve ter 9 dígitos"})
         if not celular_valido(data['celular']):
             raise serializers.ValidationError({'celular':"O número de celular deve seguir este modelo: 11 91234-1234 (respeitando os espaços e traço)"})
         return data
 
->>>>>>> 524d4ba (adding new validatords)
 
 
 
